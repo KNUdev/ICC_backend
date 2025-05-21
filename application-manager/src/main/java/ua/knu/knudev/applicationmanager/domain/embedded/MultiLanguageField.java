f60dd7fffd70f084a5b01bcf84a20a0f395e2d7e
faibles <maria0987051500@gmail.com>
2025-05-21 23:35:53 +0300

    [ICC-5] Implement CRUD logic

--- application-manager/src/main/java/ua/knu/knudev/applicationmanager/domain/embedded/MultiLanguageField.java
+++ application-manager/src/main/java/ua/knu/knudev/applicationmanager/domain/embedded/MultiLanguageField.java
@@ -2,25 +2,17 @@ package ua.knu.knudev.applicationmanager.domain.embedded;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 public class MultiLanguageField {
-
     @Column(nullable = false)
     private String en;
 
     @Column(nullable = false)
     private String uk;
-
-    public MultiLanguageField() {
-    }
-
-    public MultiLanguageField(String en, String uk) {
-        this.en = en;
-        this.uk = uk;
-    }
 }
--- application-manager/src/main/java/ua/knu/knudev/applicationmanager/mapper/ApplicationMapper.java
+++ application-manager/src/main/java/ua/knu/knudev/applicationmanager/mapper/ApplicationMapper.java
@@ -1,38 +1,10 @@
 package ua.knu.knudev.applicationmanager.mapper;
 
+import org.mapstruct.Mapper;
 import ua.knu.knudev.applicationmanager.domain.Application;
-import ua.knu.knudev.applicationmanager.domain.Department;
-import ua.knu.knudev.applicationmanager.dto.ApplicationDto;
+import ua.knu.knudev.applicationmanagerapi.dto.ApplicationDto;
+import ua.knu.knudev.icccommon.mapper.BaseMapper;
 
-public class ApplicationMapper {
-
-    public static Application toEntity(ApplicationDto dto, Department department) {
-        return Application.builder()
-                .id(dto.getId())
-                .applicantName(dto.getApplicantName())
-                .email(dto.getEmail())
-                .receivedAt(dto.getReceivedAt())
-                .completedAt(dto.getCompletedAt())
-                .problemDescription(dto.getProblemDescription())
-                .problemPhoto(dto.getProblemPhoto())
-                .status(dto.getStatus())
-                .Department(department)
-                .assignedEmployeeIds(dto.getAssignedEmployeeIds())
-                .build();
-    }
-
-    public static ApplicationDto toDto(Application application) {
-        return ApplicationDto.builder()
-                .id(application.getId())
-                .applicantName(application.getApplicantName())
-                .email(application.getEmail())
-                .receivedAt(application.getReceivedAt())
-                .completedAt(application.getCompletedAt())
-                .problemDescription(application.getProblemDescription())
-                .problemPhoto(application.getProblemPhoto())
-                .status(application.getStatus())
-                .departmentId(application.getDepartment() != null ? application.getDepartment().getId() : null)
-                .assignedEmployeeIds(application.getAssignedEmployeeIds())
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface ApplicationMapper extends BaseMapper<Application, ApplicationDto> {
 }
