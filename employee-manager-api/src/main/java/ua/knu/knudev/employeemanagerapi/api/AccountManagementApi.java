e03f94275f36d45bb5092e23765565026d9b4fb9
Sviatoslav Chervoniuk <94186409+Blezium@users.noreply.github.com>
2025-06-23 13:57:01 +0300

    [ICC-41] fixed employee tests (#43)

--- employee-manager-api/src/main/java/ua/knu/knudev/employeemanagerapi/api/AccountManagementApi.java
+++ employee-manager-api/src/main/java/ua/knu/knudev/employeemanagerapi/api/AccountManagementApi.java
@@ -1,12 +1,13 @@
 package ua.knu.knudev.employeemanagerapi.api;
 
+import jakarta.validation.Valid;
 import ua.knu.knudev.employeemanagerapi.request.AccountCredentialsUpdateRequest;
 import ua.knu.knudev.employeemanagerapi.request.AccountReceivingRequest;
 import ua.knu.knudev.employeemanagerapi.response.AccountReceivingResponse;
 
 public sealed interface AccountManagementApi permits EmployeeApi {
 
-    AccountReceivingResponse register(AccountReceivingRequest  request);
+    AccountReceivingResponse register(@Valid AccountReceivingRequest  request);
 
     AccountReceivingResponse updateCredentials(AccountCredentialsUpdateRequest request);
 
