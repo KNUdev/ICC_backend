c5cd6f4036f9808fa79ca005721ffe63950b8804
Sviatoslav Chervoniuk <chervonyuk2005@gmail.com>
2025-09-22 21:50:01 +0300

    [ICC-65] changed MinioProperties

--- file-service/src/main/java/ua/knu/knudev/fileservice/adapter/minio/MinioProperties.java
+++ file-service/src/main/java/ua/knu/knudev/fileservice/adapter/minio/MinioProperties.java
@@ -2,6 +2,7 @@ package ua.knu.knudev.fileservice.adapter.minio;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class MinioProperties {
     private String internalUrl;
     private String externalUrl;
+    @Value("${application.base-url}")
     private String filePathUrl;
     private String accessKey;
     private String secretKey;
