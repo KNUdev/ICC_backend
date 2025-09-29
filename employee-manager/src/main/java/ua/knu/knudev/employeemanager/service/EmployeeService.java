0b95addf3bc9cb58d1928aa8e56df8e657a8e00c
Denys <113017276+DenysLnk@users.noreply.github.com>
2025-09-29 22:17:50 +0300

    Refactor avatar file handling in EmployeeService

--- employee-manager/src/main/java/ua/knu/knudev/employeemanager/service/EmployeeService.java
+++ employee-manager/src/main/java/ua/knu/knudev/employeemanager/service/EmployeeService.java
@@ -253,10 +253,12 @@ public class EmployeeService implements EmployeeApi {
             );
             employee.setSector(sector);
         }
-        if (StringUtils.isNotBlank(request.avatarFile().getOriginalFilename())) {
-            updateAvatar(request.id(), request.avatarFile());
-        } else {
-            removeAvatar(request.id());
+        if (request.avatarFile() != null) {
+            if (StringUtils.isNotBlank(request.avatarFile().getOriginalFilename())) {
+                updateAvatar(request.id(), request.avatarFile());
+            } else {
+                removeAvatar(request.id());
+            }
         }
 
         if (request.email() != null && (!employee.getEmail().equals(request.email())
